ad0bae9a86673171a4952a41811290f006e65e00
Alon Levy <alevy@redhat.com>
2010-11-04 17:26:13 +0200

    server/reds: don't call close on NULL channel on atexit callback

--- server/reds.c
+++ server/reds.c
@@ -2187,7 +2187,9 @@ static void reds_init_ssl()
 
 static void reds_exit()
 {
-    main_channel_close(reds->main_channel);
+    if (reds->main_channel) {
+        main_channel_close(reds->main_channel);
+    }
 #ifdef RED_STATISTICS
     shm_unlink(reds->stat_shm_name);
     free(reds->stat_shm_name);
