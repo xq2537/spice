3accb60240dbfd9a2a7627aef879f403cd517612
Hans de Goede <hdegoede@redhat.com>
2011-04-01 14:19:25 +0200

    server: add discard all option to agent message filter

--- server/reds.c
+++ server/reds.c
@@ -576,7 +576,7 @@ static void reds_reset_vdp()
         ring_add(&state->read_bufs, &state->current_read_buf->link);
         state->current_read_buf = NULL;
     }
-    agent_msg_filter_init(&state->read_filter, agent_copypaste);
+    agent_msg_filter_init(&state->read_filter, agent_copypaste, FALSE);
     state->client_agent_started = FALSE;
 }
 
@@ -595,7 +595,8 @@ void reds_disconnect()
     reds->disconnecting = TRUE;
     reds->link_id = 0;
 
-    agent_msg_filter_init(&reds->agent_state.write_filter, agent_copypaste);
+    agent_msg_filter_init(&reds->agent_state.write_filter, agent_copypaste,
+                          FALSE);
     if (reds->agent_state.connected) {
         SpiceCharDeviceInterface *sif;
         sif = SPICE_CONTAINEROF(vdagent->base.sif, SpiceCharDeviceInterface, base);
@@ -3416,8 +3417,8 @@ static void init_vd_agent_resources()
     ring_init(&state->internal_bufs);
     ring_init(&state->write_queue);
     ring_init(&state->read_bufs);
-    agent_msg_filter_init(&state->write_filter, agent_copypaste);
-    agent_msg_filter_init(&state->read_filter, agent_copypaste);
+    agent_msg_filter_init(&state->write_filter, agent_copypaste, FALSE);
+    agent_msg_filter_init(&state->read_filter, agent_copypaste, FALSE);
 
     state->read_state = VDI_PORT_READ_STATE_READ_HADER;
     state->recive_pos = (uint8_t *)&state->vdi_chunk_header;
