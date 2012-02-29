4ec9ac20fce858d7fe6769b14b521b62c64c828c
Christophe Fergeau <cfergeau@redhat.com>
2012-02-29 19:01:55 +0100

    mingw: #ifdef unneeded #define in common.h
    common.h has some #define when doing win32 build to workaround a few
    missing functions on these systems. However, since mingw32 has some
    of these, this causes either warnings about redefining preprocessor
    symbols or wreak havoc in mingw headers trying to use these symbols.
    This commit wraps these symbols in an #ifndef __MINGW32__ to avoid
    using them on this platform.

--- client/common.h
+++ client/common.h
@@ -44,12 +44,14 @@
 #ifdef __GNUC__
 #define UNICODE 1
 #define _UNICODE 1
+#if !defined __MINGW32__
 #define WINVER 0x0501
 #define swprintf_s(_str, _len, _fmt, ...) \
     swprintf(_str, _fmt, ## __VA_ARGS__)
 #define vsnprintf_s(_str, _len1, _len2, _fmt, _valist) \
     vsnprintf(_str, _len2, _fmt, _valist)
 #define _ftime_s(_t) _ftime(_t)
+#endif
 #endif
 #include <winsock2.h>
 #include <windows.h>
