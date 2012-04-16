3cd2f843b5ec43a8fff3233d2659c745c8fdca3f
Aric Stewart <aric@codeweavers.com>
2012-04-16 13:10:01 -0500

    update windows client generate.bat and generate1.bat
    the python script is now in spice-common and client_marshallers.h needs
    to be included instead of marshallers.h

--- client/windows/generate.bat
+++ client/windows/generate.bat
@@ -1,2 +1,2 @@
-python ..\..\spice_codegen.py -d -c  -i common.h -i messages.h ..\..\spice.proto ..\generated_demarshallers.cpp
-python ..\..\spice_codegen.py --generate-marshallers -P --include "common.h" --include messages.h --include marshallers.h --client ..\..\spice.proto ..\generated_marshallers.cpp
+python ..\..\spice-common\spice_codegen.py -d -c  -i common.h -i messages.h ..\..\spice-common\spice.proto ..\generated_demarshallers.cpp
+python ..\..\spice-common\spice_codegen.py --generate-marshallers -P --include "common.h" --include messages.h --include client_marshallers.h --client ..\..\spice-common\spice.proto ..\generated_marshallers.cpp
--- client/windows/generate1.bat
+++ client/windows/generate1.bat
@@ -1,2 +1,2 @@
-python ..\..\spice_codegen.py -d -c  -i common.h -i messages.h --prefix 1 --ptrsize 8 ..\..\spice1.proto ..\generated_demarshallers1.cpp
-python ..\..\spice_codegen.py --generate-marshallers -P --include "common.h" --include messages.h  --include marshallers.h --client --prefix 1 --ptrsize 8 ..\..\spice1.proto ..\generated_marshallers1.cpp
+python ..\..\spice-common\spice_codegen.py -d -c  -i common.h -i messages.h --prefix 1 --ptrsize 8 ..\..\spice-common\spice1.proto ..\generated_demarshallers1.cpp
+python ..\..\spice-common\spice_codegen.py --generate-marshallers -P --include "common.h" --include messages.h  --include client_marshallers.h --client --prefix 1 --ptrsize 8 ..\..\spice-common\spice1.proto ..\generated_marshallers1.cpp
