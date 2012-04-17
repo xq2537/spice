962f239159960801d2cee06f2d583bdbb0112dda
Aric Stewart <aric@codeweavers.com>
2012-04-17 12:53:16 -0500

    Update win32 generate(1).bat files to match modern usage
    This involves:
     generating .c instead of .cpp files
     Not including "common.h"
     Standardizing some parameters
    
    Signed-off-by: Aric Stewart <aric@codeweavers.com>

--- client/windows/generate.bat
+++ client/windows/generate.bat
@@ -1,2 +1,2 @@
-python ..\..\spice-common\spice_codegen.py -d -c  -i common.h -i messages.h ..\..\spice-common\spice.proto ..\generated_demarshallers.cpp
-python ..\..\spice-common\spice_codegen.py --generate-marshallers -P --include "common.h" --include messages.h --include client_marshallers.h --client ..\..\spice-common\spice.proto ..\generated_marshallers.cpp
+python ..\..\spice-common\spice_codegen.py --generate-demarshallers  --client --include messages.h ..\..\spice-common\spice.proto ..\generated_demarshallers.c
+python ..\..\spice-common\spice_codegen.py --generate-marshallers -P --include messages.h --include client_marshallers.h --client ..\..\spice-common\spice.proto ..\generated_marshallers.c
