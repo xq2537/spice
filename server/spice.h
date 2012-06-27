343b3b43b34d890ae139e75a4fca4fdd1625797d
Alon Levy <alevy@redhat.com>
2012-06-27 17:12:03 +0300

    Revert "spice.h: bump SPICE_SERVER_VERSION for release 0.11.0"
    This reverts commit dd5c995c32cf595d31e59975bd1b3216cf996656.

--- server/spice.h
+++ server/spice.h
@@ -22,7 +22,7 @@
 #include <sys/socket.h>
 #include <spice/qxl_dev.h>
 
-#define SPICE_SERVER_VERSION 0x000b00 /* release 0.11.0 */
+#define SPICE_SERVER_VERSION 0x000a03 /* release 0.10.3 */
 
 /* interface base type */
 
