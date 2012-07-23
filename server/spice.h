ad7c5ca72a36a867c11409ebe55016a8da7b7441
Alon Levy <alevy@redhat.com>
2012-07-23 14:23:19 +0300

    bump version to 0.11.1 for new spice_qxl_monitors_config_async
    bump current and age since only additions where done.
    new server library is libspice-server.so.1.2.0 .

--- server/spice.h
+++ server/spice.h
@@ -22,7 +22,7 @@
 #include <sys/socket.h>
 #include <spice/qxl_dev.h>
 
-#define SPICE_SERVER_VERSION 0x000b00 /* release 0.11.0 */
+#define SPICE_SERVER_VERSION 0x000b01 /* release 0.11.1 */
 
 /* interface base type */
 
