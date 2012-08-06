e142bd9a611ce5980b29e8084b7d154013a16686
Yonit Halperin <yhalperi@redhat.com>
2012-08-06 21:58:39 +0300

    red_channel: set send_data.last_sent_serial in red_channel_client_set_message_serial
    red_channel_client_set_message_serial is called for setting
    the serial of the display channel messages after migration (on the
    destination side). The serial is retrieved from the migration data.

--- server/red_channel.c
+++ server/red_channel.c
@@ -1269,6 +1269,7 @@ uint64_t red_channel_client_get_message_serial(RedChannelClient *rcc)
 
 void red_channel_client_set_message_serial(RedChannelClient *rcc, uint64_t serial)
 {
+    rcc->send_data.last_sent_serial = serial;
     rcc->send_data.serial = serial;
 }
 
