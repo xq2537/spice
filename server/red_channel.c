df2603655202e5f9a7a6b45a2b2b8609128fdf16
Yonit Halperin <yhalperi@redhat.com>
2013-05-01 13:01:43 -0400

    red_channel: stop sending ping messages after migration has completed
    We mustn't send any msg to the client, besides MSG_MIGRATE_DATA, after
    we send MSG_MIGRATE.

--- server/red_channel.c
+++ server/red_channel.c
@@ -873,6 +873,11 @@ static void red_channel_client_default_disconnect(RedChannelClient *base)
 
 void red_channel_client_default_migrate(RedChannelClient *rcc)
 {
+    if (rcc->latency_monitor.timer) {
+        red_channel_client_cancel_ping_timer(rcc);
+        rcc->channel->core->timer_remove(rcc->latency_monitor.timer);
+        rcc->latency_monitor.timer = NULL;
+    }
     red_channel_client_pipe_add_type(rcc, PIPE_ITEM_TYPE_MIGRATE);
 }
 
