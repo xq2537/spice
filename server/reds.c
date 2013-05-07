313e2622d91675b15a6231151f2e9509ccf1dc96
Yonit Halperin <yhalperi@redhat.com>
2013-05-07 13:50:20 -0400

    reds: fix memory leak when core->base.minor_version < 3

--- server/reds.c
+++ server/reds.c
@@ -196,10 +196,9 @@ static void reds_stream_push_channel_event(RedsStream *s, int event)
 
 void reds_handle_channel_event(int event, SpiceChannelEventInfo *info)
 {
-    if (core->base.minor_version < 3 || core->channel_event == NULL)
-        return;
+    if (core->base.minor_version >= 3 && core->channel_event != NULL)
+        core->channel_event(event, info);
 
-    core->channel_event(event, info);
     if (event == SPICE_CHANNEL_EVENT_DISCONNECTED) {
         free(info);
     }
