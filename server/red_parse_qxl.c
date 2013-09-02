79896440928600bfa45cdc86e088e1196482511a
Christophe Fergeau <cfergeau@redhat.com>
2013-09-02 17:58:26 +0200

    red_parse_qxl: Change spice_error() to spice_warning()
    After eb09c25c, red_parse_qxl.c still has some spice_error() which
    will kill the server even though the code is trying to return an error
    when the spice_error() is hit.
    This commit replaces these occurrences with a spice_warning() which
    will not kill spice-server.

--- server/red_parse_qxl.c
+++ server/red_parse_qxl.c
@@ -486,7 +486,8 @@ static SpiceImage *red_get_image(RedMemSlotInfo *slots, int group_id,
         red_put_data_chunks(&chunks);
         break;
     default:
-        spice_error("unknown type %d", red->descriptor.type);
+        spice_warning("unknown type %d", red->descriptor.type);
+        goto error;
     }
     return red;
 error:
@@ -1001,7 +1002,7 @@ static int red_get_native_drawable(RedMemSlotInfo *slots, int group_id,
                               &red->u.whiteness, &qxl->u.whiteness, flags);
         break;
     default:
-        spice_error("unknown type %d", red->type);
+        spice_warning("unknown type %d", red->type);
         error = 1;
         break;
     };
@@ -1087,7 +1088,7 @@ static int red_get_compat_drawable(RedMemSlotInfo *slots, int group_id,
                               &red->u.whiteness, &qxl->u.whiteness, flags);
         break;
     default:
-        spice_error("unknown type %d", red->type);
+        spice_warning("unknown type %d", red->type);
         error = 1;
         break;
     };
